cd1413a973bc9661925f723749b3dd2ad4e4fae1
Immanuel <xavio.delta2495@gmail.com>
2023-11-15 18:54:26 +0530

    :red: Deleted exp_5/e5_5.c

--- exp_5/e5_5.c
+++
@@ -1,6 +0,0 @@
-/*Read a square matrix and an element e in it, write a program to print the
-elements adjacent to e. Ensure the following:
-a) e should be present in the matrix
-b) If e is a boundary elements, do not overshoot/undershoot the
-indices*/
-
