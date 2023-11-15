7cd8090c355a061cf58d32df742dd8842df0fa2a
Immanuel <xavio.delta2495@gmail.com>
2023-11-15 19:35:07 +0530

    🔥Final Update on e5_5.c

--- exp_5/e5_5.c
+++ exp_5/e5_5.c
@@ -1,40 +1,55 @@
-/*Read a square matrix and an element e in it, write a program to print the
+/*Read a square a and an element e in it, write a program to print the
 elements adjacent to e. Ensure the following:
-a) e should be present in the matrix
+a) e should be present in the a
 b) If e is a boundary elements, do not overshoot/undershoot the
 indices*/
 
 #include <stdio.h>
 int main()
 {
-    int a[3][3];
-    printf("\nTo find adjacent element in 3x3 matrix\n");
-
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
+    int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    printf("\nTo find adjacent element in 3x3 a\n");
+    /*
+        for (int i = 0; i < 3; i++)
         {
-            printf("\nEnter value for a[%d][%d]:", i, j);
-            scanf("%d", &a[i][j]);
-        }
-    }
-    int val;
-    printf("\nEnter the element to search adjacent for:");
-    scanf("%d", &val);
-    int k,m;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if( val == a[i][j]){
-                k=i;
-                m=j;
+            for (int j = 0; j < 3; j++)
+            {
+                printf("\nEnter value for a[%d][%d]:", i, j);
+                scanf("%d", &a[i][j]);
             }
         }
-    }
+    */
+    int e;
+    printf("\nEnter the element to search adjacent for:");
+    scanf(" %d", &e);
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; ++i)
     {
-
+        for (int j = 0; j < 3; ++j)
+        {
+            if (a[i][j] == e)
+            {
+                // Print adjacent elements if they exist
+                if (i > 0)
+                    printf("Above: %d\n", a[i - 1][j]);
+                if (i < 2)
+                    printf("Below: %d\n", a[i + 1][j]);
+                if (j > 0)
+                    printf("Left: %d\n", a[i][j - 1]);
+                if (j < 2)
+                    printf("Right: %d\n", a[i][j + 1]);
+                if (i == j)
+                {
+                    printf("Diagonal: %d,%d,%d,%d", a[0][0], a[0][2], a[2][0], a[2][2]);
+                    break;
+                }
+                if (i + j == 2 || i + j == 0)
+                    printf("Diagonal: %d\n", a[1][1]);
+                if (i + j == 1)
+                    printf("Diagnonal: %d, %d", a[j][i], a[i + 1][j + 1]);
+                if (i + j == 3)
+                    printf("Diagnonal: %d, %d", a[j][i], a[i - 1][j - 1]);
+            }
+        }
     }
 }
