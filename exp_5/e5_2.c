8cc7943bc2ed451b211d86e134b14409edfcbf45
Immanuel <xavio.delta2495@gmail.com>
2023-11-15 19:51:06 +0530

    ⭐

--- exp_5/e5_2.c
+++ exp_5/e5_2.c
@@ -3,20 +3,20 @@
 present and absentees for a day. */
 
 #include <stdio.h>
-int main()
+void main()
 {
     int n, p = 0, a = 0;
     char c;
     printf("\nEnter the number of students:");
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
+    scanf("%d", &n);//user input on number of students in the class
+    for (int i = 1; i <= n; i++)//iterating till n value
     {
         printf("student %d - ", i);
         scanf(" %c", &c); // getting user input ('P', 'A')
-        (c == 'P' || c == 'p') ?++p:++a;
+        (c == 'P' || c == 'p') ?++p:++a;//ternary operator to check for 'P' or 'p'
     }
 
     printf("\nThe number of students present is: %d", p); // printing no. of students present
     printf("\nThe number of students absent is: %d", a);  // printing no. of students absent
-    return 0;
+
 }
