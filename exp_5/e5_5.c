7c4eb4cf8ca85454fb46ceb14afc9221c28c3de6
Immanuel <xavio.delta2495@gmail.com>
2023-11-15 15:49:31 +0530

    😑 Update on e5_5.c

--- exp_5/e5_5.c
+++ exp_5/e5_5.c
@@ -4,35 +4,3 @@ a) e should be present in the matrix
 b) If e is a boundary elements, do not overshoot/undershoot the
 indices*/
 
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-    int a[3][3], b[9];
-    printf("\nTo find adjacent element in 3x3 matrix\n");
-
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("\nEnter value for a[%d][%d]:", i, j);
-            scanf("%d", &a[i][j]);
-            b[i + j] = a[i][j];
-        }
-    }
-    int val;
-    printf("\nEnter the element to search adjacent for:");
-    scanf("%d", &val);
-    for (int i = 0; i < 9; i++)
-    {
-        if (b[i] == val)
-        {
-            printf("Value exists, proceeding to find adjacent");
-        }
-        else{
-            printf("Value not in given matrix input... terminating program");
-            exit(1);
-            
-        }
-    }
-}
