d33533a2e18077884a0e3a7c7097d9d560668efa
Immanuel <xavio.delta2495@gmail.com>
2023-11-20 20:02:02 +0530

    🚀e7_5

---
+++ e7_5.c
@@ -0,0 +1,4 @@
+/*Create  a  structure  subject  <sub_code,title,credits>.  Include  this  subject 
+as a nested structure in student as an array of size 5. Generate a grand 
+grade sheet of a student with student details, subject details, grade and 
+GPA (as per grading system in ex 4). */
