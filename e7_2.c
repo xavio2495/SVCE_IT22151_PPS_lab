f538a813143108d125dc90c10bfa3c814b2eacff
Immanuel <xavio.delta2495@gmail.com>
2023-11-20 20:01:06 +0530

    🚀e7_2

---
+++ e7_2.c
@@ -0,0 +1,4 @@
+/*Create  a  structure  STUDENT  with  <Roll_no,Name,Gender,marks[5], 
+grades[5],GPA>.  Calculate  the  grade  of  each  subject  (assuming  one-to-
+one  mapping  of  marks  and  grades)  and  GPA  as  the  average  of  marks 
+scored by a student.*/
